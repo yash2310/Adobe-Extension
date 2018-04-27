c579da02605bb3c23f806fc20ccee6db01160c3b
yash kumar prajapati <yash@bluepi.in>
2018-04-27 13:56:11 +0530

    Task: Initial Commit

---
+++ FirstDemo/ExtensionContent/jsx/InDesign.jsx
@@ -0,0 +1,12 @@
+$._ext_IDSN={
+    run : function() {
+    
+    	/**********  Replace below sample code with your own JSX code  **********/
+        var appName;	    
+	    appName = "Hello InDesign";	    
+        alert(appName);
+        /************************************************************************/
+        
+        return appName;
+    },
+};
